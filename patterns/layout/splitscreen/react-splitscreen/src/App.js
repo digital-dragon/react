560ade46d67fe5b8230136ef1c2ba2a8ccbf8259
Digital Dragon <rrajendra@yahoo.com>
2025-12-19 14:33:41 -0500

    Added split screen layout

--- patterns/layout/splitscreen/react-splitscreen/src/App.js
+++ patterns/layout/splitscreen/react-splitscreen/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import SplitScreen from './SplitScreen';
+
+const Left = () => <h4>Left</h4>
+const Right = () => <h4>Right</h4>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello React!
+    <div>
+      <header style={{ background: "lightgray", fontSize: "20px"}}>
+        Welcome to React Layout Pattern!
+        <SplitScreen left={Left} right={Right}/>
       </header>
     </div>
   );
